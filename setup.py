e38084356ba36e9f696e8ba3049a626850f8f7ac
Cosmin Luță <q4break@gmail.com>
2012-10-23 16:41:24 +0300

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='shaunsephton.recipe.uwsgi',
-    version='0.0.9',
+    name='buildout.recipe.uwsgi',
+    version='0.0.10',
     description='Buildout recipe downloading, compiling and configuring uWSGI.',
     long_description = open('README.rst', 'r').read() + open('AUTHORS.rst', 'r').read() + open('CHANGELOG.rst', 'r').read(),
     author='Shaun Sephton',
     author_email='shaunsephton@gmail.com',
     license='BSD',
-    url='http://github.com/shaunsephton/shaunsephton.recipe.uwsgi',
+    url='http://github.com/lcosmin/buildout.recipe.uwsgi',
     packages = find_packages(),
     include_package_data=True,
     classifiers = [
@@ -24,5 +24,5 @@ setup(
         'zc.recipe.egg',
     ],
     zip_safe=False,
-    entry_points = {'zc.buildout': ['default = shaunsephton.recipe.uwsgi:UWSGI']},
+    entry_points = {'zc.buildout': ['default = buildout.recipe.uwsgi:UWSGI']},
 )
